fd6b4b7bcb78707a9b6e6b97a268ada77c1aeee2
cyendra <g1483335912@gmail.com>
2014-07-07 12:20:01 +0800

    中文

--- MachineLearningInAction/kNN.py
+++ MachineLearningInAction/kNN.py
@@ -1,3 +1,4 @@
+# coding=gbk
 from numpy import *
 import operator
 
@@ -7,20 +8,30 @@ def createDataSet():
     return group, labels
 
 def classify0(inX, dataSet, labels, k):
+    """
+    k-邻近算法
+    Args:
+        inX: 用于分类的输入向量
+        dataSet: 输入的训练样本集
+        labels: 标签向量
+        k: 用于选择最邻近的数目
+    Returns:
+        发生频率最高的元素标签
+    """
     dataSetSize = dataSet.shape[0]
-    print "dataSetSize=", dataSetSize
-    print "tile=", tile(inX, (dataSetSize, 1))
+    #print "dataSetSize=", dataSetSize
+    #print "tile=", tile(inX, (dataSetSize, 1))
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    print "diffMat=", diffMat
+    #print "diffMat=", diffMat
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
-    print "distances=", distances
+    #print "distances=", distances
     sortedDistIndicies = distances.argsort()
     classCount={}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel]=classCount.get(voteIlabel,0) + 1
-    print "classCount", classCount
+    #print "classCount", classCount
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
