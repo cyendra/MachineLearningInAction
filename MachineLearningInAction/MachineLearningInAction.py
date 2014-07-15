563d428a2e535011703f3d3985c2eb7da61f07aa
cyendra <g1483335912@gmail.com>
2014-07-15 20:43:22 +0800

    logRegers plt

--- MachineLearningInAction/MachineLearningInAction.py
+++ MachineLearningInAction/MachineLearningInAction.py
@@ -7,24 +7,8 @@ import treePlotter
 
 import bayes
 
-listOPosts, listClasses = bayes.loadDataSet()
+import logRegres
 
-myVocabList = bayes.createVocabList(listOPosts)
-print myVocabList
-
-print bayes.setOfWords2Vec(myVocabList, listOPosts[0])
-
-print bayes.setOfWords2Vec(myVocabList, listOPosts[3])
-
-trainMat = []
-for postinDoc in listOPosts:
-    trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
-
-p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
-print pAb
-print p0V
-print p1V
-
-print "--------------------"
-
-bayes.testingNB()
+dataArr, labelMat = logRegres.loadDataSet("testSet.txt")
+weights = logRegres.gradAscent(dataArr, labelMat)
+logRegres.plotBestFit(dataArr, labelMat, weights)
--- MachineLearningInAction/logRegres.py
+++ MachineLearningInAction/logRegres.py
@@ -1,3 +1,6 @@
+# coding=gbk
+
+from numpy import *
 
 def loadDataSet(filename):
     """
@@ -23,19 +26,53 @@ def sigmoid(inX):
 
 def gradAscent(dataMatIn, classLabels):
     """
-    梯度上升
+    梯度上升算法
     Args:
-        dataMatIn: 数据集，二维NumPy数组，每列代表不同的特征，每行代表训练样本
+        dataMatIn: 数据集，二维矩阵，每列代表不同的特征，每行代表训练样本
         classLabels: 标签集，类别标签，是一个一维向量
+    Returns:
+        weights: 回归系数
     """
-    dataMatrix = mat(dataMatIn)
-    labelMat = mat(classLabels).transpose()
-    m, n = shape(dataMatrix)
-    alpha = 0.001
-    maxCycles = 500
-    weights = ones((n, 1))
+    dataMatrix = mat(dataMatIn) # 数据样本矩阵
+    labelMat = mat(classLabels).transpose() # 标签矩阵的转置
+    m, n = shape(dataMatrix) # 读取矩阵的大小 m * n，m 是行数，n是列数
+    alpha = 0.001 # 向目标移动的步长
+    maxCycles = 500 # 迭代次数
+    weights = ones((n, 1)) # 创建 n*1 的数组
     for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)
+        h = sigmoid(dataMatrix * weights) # 矩阵相乘，得到一个 m * 1 的矩阵
         error = (labelMat - h)
-        weights = weights - alpha * dataMatrix.transpose() * error
+        weights = weights + alpha * dataMatrix.transpose() * error # 梯度上升算法的迭代公式
     return weights
+
+def plotBestFit(dataMatIn, classLabels, wei):
+    """
+    画出数据集和Logistic回归最佳拟合直线的函数
+    """
+    import matplotlib.pyplot as plt
+    weights = wei.getA()
+    dataMat, labelMat = dataMatIn, classLabels
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s = 30, c = 'red', marker = 's')
+    ax.scatter(xcord2, ycord2, s = 30, c = 'green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x,y)
+    plt.xlabel('X1'); plt.ylabel('X2')
+    plt.show()
+
+
+
+
+
+
