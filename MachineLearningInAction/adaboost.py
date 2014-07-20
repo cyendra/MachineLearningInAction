5dd966548555b0fe190ce91322a49969a24e243b
cyendra <g1483335912@gmail.com>
2014-07-20 21:22:58 +0800

    ada

--- MachineLearningInAction/adaboost.py
+++ MachineLearningInAction/adaboost.py
@@ -12,3 +12,25 @@ def loadSimpData():
     return datMat, classLabels
 
 
+def adaBoostTrainDS(dataArr, classLabels, numIt = 40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1))/m)
+    aggClasEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print "D:", D.T
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print "classEst: ", classEst.T
+        expen = multiply(-1*alpha*mat(classLabels).T,classEst)
+        D = multiply(D,exp(expon))
+        D = D/D.sum()
+        aggClassEst += alpha * classEst
+        print "aggClassEst: ", aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst)!=mat(classLabels).T, ones((m,1)))
+        errorRate = aggErrors.sum()/m
+        print "total error: ", errorRate, "\n"
+        if errorRate == 0.0: break
+    return weakClassArr
